9b9f37e43c3b5c26e773e4a2f8d26a788e970eec
Daffa Daris Mahendra Ansori <dmadaffa45@gmail.com>
2026-07-13 17:11:21 +0700

    fix: use web-compatible libsql driver adapter for Vercel Serverless environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { PrismaLibSql } from '@prisma/adapter-libsql/web';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
